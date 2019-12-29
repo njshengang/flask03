fafb180f8aae286dda5cbb0dde446ad44b537c98
njshengang <njshengang@yeah.net>
2019-12-29 21:17:26 +0800

    20191228

---
+++ test1.py
@@ -0,0 +1,22 @@
+from random import randint
+
+def play():
+    random_int =randint(0,100)
+    while True:
+        user_guess=int(input("what number you guess(0-100):"))
+        if user_guess==random_int:
+            print("you found the number({random_int}),congrats!")
+            break
+        if user_guess>random_int:
+            print("your number is more than  the number we guessed!")
+            continue
+        if user_guess<random_int:
+            print("your number is less than  the number we guessed!")
+            continue
+
+
+
+if __name__=="__main__":
+    play()
+
+
